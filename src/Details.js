918f526a56a280b1c87a93bd7bf08fc32e319639
iamharpar <iamharpar1@gmail.com>
2020-03-12 17:21:15 +0530

    class components
    proper Details page using class components but before configuring babel

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,43 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default function Details() {
-  return <h1>Hi, lol.</h1>;
+    this.state = {
+      loading: true
+    };
+  }
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breeds: animal.breeds.primary,
+        loading: false
+      });
+    }, console.error);
+  }
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+
+    const { name, animal, breeds, location, description } = this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breeds} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default Details;
